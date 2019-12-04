3a945beb0ef4fe530c52a568f60adec9cc67e225
yoshiking <yoshiking0711@gmail.com>
2019-12-04 18:04:36 +0900

    add web-app

--- src/service/main.py
+++ src/service/main.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
+import sqlite3
+import json
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    conn = sqlite3.connect('./database/datas.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS lists (expired_date, name)')
+    
     if request.method == 'POST':
-        with open('./database/data', 'a') as f:
-            f.write(request.form['something'] + ' ' + request.form['date'] + '\n')
-    with open('./database/data', 'r') as f:
-        datas = f.read().split('\n')
+        c.execute('INSERT INTO lists VALUES (?, ?)', (request.form['something'], request.form['date']))
+    datas = c.execute('SELECT * FROM lists')
+    datas = json.dumps(list(datas))
+    print("[+] datas:", datas)
+    conn.commit()
     return render_template('index.html', datas=datas)
 
 if __name__=="__main__":
